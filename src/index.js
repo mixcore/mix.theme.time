f8c5c03658a5f2ac635324473ba481a684d69c2d
Huy Nguyen <Smilefounder@users.noreply.github.com>
2022-07-17 22:54:43 +0700

    init source

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Import all plugins
+import * as bootstrap from 'bootstrap';
+
+// Or import only needed plugins
+import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
