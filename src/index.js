b1918d5753e75aebfcc2c780a251c1af2533ae1a
Huy Nguyen <Smilefounder@users.noreply.github.com>
2022-07-22 18:44:26 +0700

    init sveltekit + tailwindcss

--- src/index.js
+++
@@ -1,5 +0,0 @@
-// Import all plugins
-import * as bootstrap from 'bootstrap';
-
-// Or import only needed plugins
-import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
